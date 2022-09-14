c0f90954cd45d7be8c0ec0f13a3e98d5086cfe31
Arnau Garcia Cases <60266982+arnaugcases@users.noreply.github.com>
2022-09-14 16:57:26 +0200

    Projects info complete

--- assets/js/myJs.js
+++ assets/js/myJs.js
@@ -1,7 +1,23 @@
-function seeMoreFcn() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
+function seeMoreFcn1() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("myBtn1");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  function seeMoreFcn2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("myBtn2");
   
     if (dots.style.display === "none") {
       dots.style.display = "inline";
